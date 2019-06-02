7e8ca7f855b055719ea1ff86b0122994154d428a
Leopold0801 <982539404@qq.com>
2019-06-02 18:33:32 +0800

    commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import tensorflow as tf
+import numpy as np
+from PIL import Image
+import mnist_inference
+import mnist_train
+
+
+def restore_model(testPicArr):
+    with tf.Graph().as_default() as tg:
+        x = tf.placeholder(tf.float32,[1,mnist_inference.IMAGE_SIZE,
+                                        mnist_inference.IMAGE_SIZE,mnist_inference.NUM_CHANNELS])
+        y = mnist_inference.inference(x,False,None)
+        preValue = tf.argmax(y,1)
+
+        variable_averages = tf.train.ExponentialMovingAverage(mnist_train.MOVING_AVERAGE_DECAY)
+        variable_to_restore = variable_averages.variables_to_restore()
+        saver = tf.train.Saver(variable_to_restore)
+
+        with tf.Session() as sess:
+            ckpt = tf.train.get_checkpoint_state(mnist_train.MODEL_SAVE_PATH)
+            if ckpt and ckpt.model_checkpoint_path:
+                saver.restore(sess,ckpt.model_checkpoint_path)
+
+                preValue = sess.run(preValue,feed_dict={x:testPicArr})
+                return preValue
+            else:
+                print("No checkpoint file found!")
+                return -1
+
+
+def pre_img(PicName):
+    img = Image.open(PicName)
+    reIm = img.resize((28,28),Image.ANTIALIAS)
+    im_arr = np.array(reIm.convert('L'))
+    threshold = 50
+    for i in range(28):
+        for j in range(28):
+            im_arr[i][j] = 255-im_arr[i][j]
+            if (im_arr[i][j] < threshold):
+                im_arr[i][j] = 0
+            else:
+                im_arr[i][j] = 255
+
+    nm_arr = im_arr.reshape([1,784])
+    nm_arr = nm_arr.astype(np.float32)
+    img_ready = np.multiply(nm_arr,1.0/255.0)
+    img_ready1 = np.reshape(img_ready,(1,mnist_inference.IMAGE_SIZE,
+                                        mnist_inference.IMAGE_SIZE,mnist_inference.NUM_CHANNELS))
+
+    return img_ready1
+
+def app_run():
+    testNum = int(input('输入要测试的图片数量:'))
+    for i in range(testNum):
+        testPic = input("输入图片路径:")
+        true_val = testPic.split('/')[-1].split('.')[-2]
+        testPicArr = pre_img(testPic)
+        prevalue = restore_model(testPicArr)
+        #print(true_val,int(prevalue))
+        if int(true_val) == int(prevalue):
+            flg = '识别正确'
+        else:
+            flg = '识别错误'
+        #print(flg)
+        print('识别后的输出是:',int(prevalue) , ','+ flg)
+
+def main():
+    app_run()
+
+if __name__=='__main__':
+    main()
---
+++ mnist_train.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import os
+import numpy as np
+import tensorflow as tf
+
+old_v = tf.logging.get_verbosity()
+tf.logging.set_verbosity(tf.logging.ERROR)
+from tensorflow.examples.tutorials.mnist import input_data
+
+#from IPython import embed
+
+# 加载mnist_inference.py中定义的常量和前向传播的函数
+import mnist_inference
+
+# 每次读取样本数
+BATCH_SIZE = 100
+LEARNING_RATE_BASE = 0.01
+LEARNING_RATE_DECAY = 0.99
+REGULARAZTION_RATE = 0.0001
+# 总的训练次数
+TRAINING_STEPS = 30000
+MOVING_AVERAGE_DECAY = 0.99
+# 模型保存的路径和文件名
+MODEL_SAVE_PATH = "model/"
+MODEL_NAME = "model.ckpt"
+
+def train(mnist):
+    # 输入数据placeholder的格式，输入为一个四维矩阵
+    x = tf.placeholder(tf.float32, [
+        BATCH_SIZE,                             # 第一维表示一个batch中样例的个数
+        mnist_inference.IMAGE_SIZE,             # 第二维和第三维表示图片的尺寸
+        mnist_inference.IMAGE_SIZE,
+        mnist_inference.NUM_CHANNELS],          # 第四维表示图片的深度，对于RBG格式的图片，深度为5
+        name='x-input')
+    y_ = tf.placeholder(tf.float32, [None, mnist_inference.OUTPUT_NODE], name='y-input')
+
+    regularizer = tf.contrib.layers.l2_regularizer(REGULARAZTION_RATE) #加入l2正则化防止过拟合
+    # 直接使用mnist_inference.py中定义的前向传播过程
+    y = mnist_inference.inference(x, True, regularizer)
+    global_step = tf.Variable(0, trainable=False)
+
+    # 定义损失函数、学习率、滑动平均操作以及训练过程
+    variable_averages = tf.train.ExponentialMovingAverage(MOVING_AVERAGE_DECAY, global_step)
+    variable_averages_op = variable_averages.apply(tf.trainable_variables())
+    cross_entropy = tf.nn.sparse_softmax_cross_entropy_with_logits(logits=y, labels=tf.argmax(y_, 1)) #������֤������
+    cross_entropy_mean = tf.reduce_mean(cross_entropy)
+    loss = cross_entropy_mean + tf.add_n(tf.get_collection('losses'))
+    learning_rate = tf.train.exponential_decay(LEARNING_RATE_BASE, global_step, mnist.train.num_examples/BATCH_SIZE, LEARNING_RATE_DECAY)
+    # 使用反向传播进行优化，最小化损失函数
+    train_step = tf.train.GradientDescentOptimizer(learning_rate).minimize(loss, global_step=global_step)
+    with tf.control_dependencies([train_step, variable_averages_op]):
+        train_op = tf.no_op(name='train')
+
+    # train.Saver是自动加断点、保存函数
+    saver = tf.train.Saver()
+    with tf.Session() as sess:
+        # 初始化全局变量
+        tf.global_variables_initializer().run()
+        for i in range(TRAINING_STEPS):
+            xs, ys = mnist.train.next_batch(BATCH_SIZE)
+            # 断点调试：embed()
+            # 将输入的训练数据格式调整为一个四维矩阵
+            reshaped_xs = np.reshape(xs, (BATCH_SIZE, mnist_inference.IMAGE_SIZE, mnist_inference.IMAGE_SIZE, mnist_inference.NUM_CHANNELS))
+            _, loss_value, step = sess.run([train_op, loss, global_step], feed_dict={x: reshaped_xs, y_: ys})
+            #每1000轮保存一次模型。
+            if i%1000 == 0:
+                # 输出当前的训练情况。这里只输出了模型在当前训练batch上的损失函数大小。通过损失函数的大小可以大概了解训练的情况。
+                print("After %d training step(s), loss on training batch is %f." % (step, loss_value))
+                # 保存当前的模型。规定了保存的格式，global_step参数，这样可以让每个被保存模型的文件名末尾加上训练的轮数，比如“model.ckpt-1000”表示训练1000轮后得到的模型
+                saver.save(sess, os.path.join(MODEL_SAVE_PATH, MODEL_NAME), global_step=global_step)
+
+
+def main(argv=None):
+    mnist = input_data.read_data_sets("dataset/", one_hot=True)
+    tf.logging.set_verbosity(old_v)
+    train(mnist)
+
+
+if __name__ == '__main__':
+    tf.app.run()
